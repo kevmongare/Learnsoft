9e5a589cc03e418e212db0c2b1442feec1cef374
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-06 22:48:33 +0300

    update on the footer to be mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -173,7 +173,7 @@ function App() {
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
         viewBox="0 0 24 24"
-        className="w-8 h-8"
+        className="w-6 h-6 md:w-8 md:h-8"
       >
         <path d="M12.04 2.01A10 10 0 0 0 2 12.06a9.84 9.84 0 0 0 1.37 5.09L2 22l5.07-1.33a9.95 9.95 0 0 0 4.96 1.28H12A10 10 0 0 0 12.04 2zM12 20.08a8.07 8.07 0 0 1-4.1-1.13l-.3-.17-3.02.79.8-2.94-.2-.31a8.04 8.04 0 1 1 14.9-4.27 8.03 8.03 0 0 1-8.08 8.03zm4.62-6.03c-.26-.13-1.5-.74-1.73-.83s-.4-.13-.57.13-.66.83-.81 1-.3.2-.56.07a6.6 6.6 0 0 1-1.94-1.2 7.4 7.4 0 0 1-1.37-1.7c-.14-.26 0-.4.12-.53.12-.13.26-.3.4-.45.14-.15.2-.26.3-.43a.5.5 0 0 0-.02-.48c-.07-.14-.57-1.37-.78-1.87s-.4-.42-.56-.43h-.48a.92.92 0 0 0-.67.31 2.78 2.78 0 0 0-.86 2.06c0 1.22.87 2.4 1 2.57.13.17 1.7 2.6 4.13 3.64.58.25 1.04.4 1.4.51a3.35 3.35 0 0 0 1.56.1 2.66 2.66 0 0 0 1.75-1.22c.22-.3.22-.54.16-.74s-.24-.17-.5-.3z" />
       </motion.svg>
@@ -196,7 +196,7 @@ function App() {
             name="EMAIL"
             placeholder="Enter your email"
             required
-            className="p-2 border border-gray-300 rounded w-100 mb-3"
+            className="p-2 border border-orange-500 rounded w-80 bg-white md:w-100 mb-3 text-black"
           />
           <button
             type="submit"
@@ -209,7 +209,7 @@ function App() {
       
         <hr className='max-w-7xl mx-auto my-5 text-gray-400'/>
         <footer className='justify-between flex mx-auto max-w-6xl'>
-        <p className='text-sm text-start font-extralight max-w-6xl  text-gray-400'>&copy; Learnsoft Beliotech Solutions Limited, All Right Reserved. 2025</p>
+        <p className=' text-[8px] md:text-sm text-start font-extralight max-w-6xl  text-gray-400'>&copy; Learnsoft Beliotech Solutions Limited, All Right Reserved. 2025</p>
         <div>
           <a href="" className='text-gray-400'>FAQ's</a>
         </div>
